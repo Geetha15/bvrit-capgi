cc7d322faabd24ecf0cf86f0e30951559d20b9bc
Banoth geetha <Banoth geetha@LAPTOP-C62KU9LS>
2020-02-19 23:14:51 +0530

    project

---
+++ lab-7/src/com/capg/labs/Exa2.java
@@ -0,0 +1,24 @@
+package com.capg.labs;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Exa2 {
+    public static Map<Character,Integer> countCharacter(){
+    		Scanner s=new Scanner(System.in);
+    		int t=s.nextInt();
+    		char n[]=new char[t];
+    		
+    		for(int i=0;i<t;i++){
+    			 //n1[i]=s.nextInt();
+    			n[i]=s.charAt(i);
+    		}
+    			}
+    		
+    	
+	public static void main(String[] args) {
+		HashMap<Character,Integer> h=new HashMap();
+
+	}
+
+}
